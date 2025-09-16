688c9fcde99bb278996c63d4434277b6790cd20c
jh4995 <100337429+jh4995@users.noreply.github.com>
2025-09-17 00:50:59 +0900

    delete DATA_PATH

--- welcometogithub-jh4995/count.py
+++ welcometogithub-jh4995/count.py
@@ -1,6 +1,6 @@
 import os
 
-DATA_PATH = r"C:\Users\82104\OneDrive\바탕 화면\충남대 자료\2025-2\알고리즘\167.과학기술표준분류 대응 특허 데이터\01-1.정식개방데이터\Training\01.원천데이터\unzipped"
+DATA_PATH = r""
 
 if __name__ == "__main__":
     
@@ -17,4 +17,5 @@ if __name__ == "__main__":
                 json_count += 1
     print(f"전체 폴더 개수: {folder_count}")
     print(f"JSON 파일 개수: {json_count}")
+
     print(f"전체 파일 크기: {total_size} 바이트")
