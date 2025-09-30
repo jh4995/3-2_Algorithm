b698f1424ceea4ad0a46fa361f8cd542ed4932a6
[jh4995] <[jh5214995@naver.com]>
2025-09-30 21:40:46 +0900

    4th week upload

---
+++ indexer-jh4995/main.py
@@ -0,0 +1,17 @@
+# main.py
+from src.indexer import Indexer
+
+# 설정
+DATA_DIR = r"C:\Users\82104\OneDrive\바탕 화면\충남대 자료\2025-2\알고리즘\167.과학기술표준분류 대응 특허 데이터\01-1.정식개방데이터\Training\01.원천데이터\unzipped"  # data path
+INDEX_DIR = "index"  # indexer 출력 dir
+DOC_TABLE_FILE = "doc_table.json"
+TERM_DICT_FILE = "term_dict.json"
+POSTINGS_FILE = "postings.bin"
+
+if __name__ == "__main__":
+    task = input("작업을 선택하세요 (index/search): ").strip().lower()
+    
+    if task in ("index", "i"):
+        indexer = Indexer(DATA_DIR, INDEX_DIR, DOC_TABLE_FILE, 
+                         TERM_DICT_FILE, POSTINGS_FILE)
+        indexer.build_index()
