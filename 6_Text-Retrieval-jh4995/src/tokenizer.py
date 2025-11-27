6bb7063e58ee115b9dec822220ab4dad3ecbcf6e
[jh4995] <[jh5214995@naver.com]>
2025-11-27 21:59:34 +0900

    reconnect repository

---
+++ 6_Text-Retrieval-jh4995/src/tokenizer.py
@@ -0,0 +1,8 @@
+from konlpy.tag import Komoran
+
+tagger = Komoran()
+
+def extract_terms(text):
+    tokens = tagger.pos(text)
+    return [w.lower() if t == 'SL' else w for w, t in tokens
+            if t in {'NNG', 'NNP', 'SL'}]
